e28b2d313676375ed8b11bcac2f7febef720b660
yuriyfomin17 <fominyura2012@gmail.com>
2020-08-18 21:51:02 +0100

    Updates

---
+++ precache-manifest.06d2c108f89679adace29cb3d2dc86be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daced7ede047389e24aa31a8174493d2",
+    "url": "/my-to-do-redux/index.html"
+  },
+  {
+    "revision": "37dfc7bf7e5b740f0616",
+    "url": "/my-to-do-redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b83e81e8f3b5b922d43",
+    "url": "/my-to-do-redux/static/js/2.b127b6f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-to-do-redux/static/js/2.b127b6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dfc7bf7e5b740f0616",
+    "url": "/my-to-do-redux/static/js/main.0c6b8047.chunk.js"
+  },
+  {
+    "revision": "84cdc048679f9e5577cf",
+    "url": "/my-to-do-redux/static/js/runtime-main.8ca1c23c.js"
+  }
+]);
